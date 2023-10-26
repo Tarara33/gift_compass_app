184371bb30401fc08dd27dfd5ec82aa9e91fe31f
Sarina <sarina0618@gmail.com>
2023-10-27 04:18:39 +0900

    🚧 errors作成...flash作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = '位置情報を登録しました'
+      redirect_back_or_to root_path, status: :see_other
+    else
+      flash.now[:danger] = '失敗'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
