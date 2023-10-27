e2d084e3861677c6aa37615c243e447e2b1cd280
Sarina <sarina0618@gmail.com>
2023-10-27 10:51:51 +0900

    🚧 item modelバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+
+  enum price_range:{ 0_1000: 0, 1000_3000: 1, 3000_6000: 2, 6000_10000: 3, 10000_15000: 4, 15000_20000: 5, 20000_25000: 6,
+                    25000_30000:  7, above_30000: 8}
+  enum target_gender:{ other: 0, man: 1, woman:2 }
 end
