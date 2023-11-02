29ad525eb688fdf5eaf2867bf1845895473860e1
Sarina <sarina0618@gmail.com>
2023-11-03 00:03:13 +0900

    ✅ system spec item ログイン機能追加後作る

--- spec/system/items_spec.rb
+++ spec/system/items_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Items", type: :system do
-  before do
-    driven_by(:rack_test)
+  let(:user) { create(:user) }
+  
+  describe '成功' do
+    context '必須項目が全て有効な場合' do
+      it '欲しいものの登録に成功する' do
+        visit new_item_path
+        fill_in 'item[item_name]', with: 'テスト'
+        fill_in 'item[price]', with: 1200
+        select '0~1000円', from: 'item[price_range]'
+        select 'みんなにオススメ', from: 'item[target_gender]'
+        execute_script("document.querySelector('.fixed-bottom').style.display = 'none';")
+        click_button '登録する'
+        expect(page).to have_content('欲しいもの登録に成功しました')
+        expect(current_path).to eq(item_path(Item.last.id))
+      end
+    end
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
 end
