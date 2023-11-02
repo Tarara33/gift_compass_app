97fae11d29a556b03cc0c5e84fa782d4c758a81d
Sarina <sarina0618@gmail.com>
2023-11-02 20:08:28 +0900

    ✨ 基本的なitemCRUD作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
 
-  validates :item_name, presence: true
   validates :price, presence: true, numericality: {greater_than_or_equal_to: 0}
+  with_options presence: true do
+    validates :item_name, :price_range, :target_gender
+  end
 
   enum price_range:{ upto_1000: 0, upto_3000: 1, upto_6000: 2, upto_10000: 3, upto_15000: 4, upto_20000: 5, upto_25000: 6,
                     upto_30000:  7, above_30000: 8}
--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :item do
-    sequence(:name) {|n| "アイテム#{n}" }
+    sequence(:item_name) {|n| "アイテム#{n}" }
     price { 1000 }
     price_range { :upto_1000 }
     target_gender { :other }
-    association user
+    association :user
   end
 end
