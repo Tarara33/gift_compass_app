6fec9709e58c28836dae348c75d55b23e56d7eec
Sarina <sarina0618@gmail.com>
2023-11-02 20:35:39 +0900

    🧪 system spec users, items追加

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  describe '成功' do
+    context 'ユーザー登録フォームの入力が有効な場合' do
+      it 'ユーザー登録に成功する' do
+        
+      end
+    end
+  end
+end
