47eed3825d52b3286e650d7d453706cd81078bff
Sarina <sarina0618@gmail.com>
2023-11-14 05:01:36 +0900

    ✨ ADD: カウンターキャッシュ追加

--- app/models/item_tag_relation.rb
+++ app/models/item_tag_relation.rb
@@ -1,6 +1,6 @@
 class ItemTagRelation < ApplicationRecord
   belongs_to :item
-  belongs_to :tag
+  belongs_to :tag, counter_cache: :items_count
 
   validates :item_id, uniqueness: {scope: :tag_id}
 end
---
+++ db/migrate/20231113195216_add_items_count_to_tags.rb
@@ -0,0 +1,5 @@
+class AddItemsCountToTags < ActiveRecord::Migration[7.0]
+  def change
+    add_column :tags, :items_count, :integer, default: 0, null: false
+  end
+end
