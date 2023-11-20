a27a8ec33f88c8015e60700c7c0ef12b1ed76995
Sarina <sarina0618@gmail.com>
2023-11-21 02:55:19 +0900

    🚧 :矢印ボタン追加...場面ごとに切り替える

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def next
+    Item.where("id < ?", self.id).order("id DESC").first
+  end
+
+  def previous
+    Item.where("id > ?", self.id).order("id ASC").first
+  end
 end
