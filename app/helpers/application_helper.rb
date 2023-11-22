ad7a96806f529d9cefdc29b6408e5c89368857a5
Sarina <sarina0618@gmail.com>
2023-11-23 08:52:49 +0900

    ✨ADD: 動的タイトル実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def page_title(page_title = '')
+    default_title = "Gift Conpass"
+    page_title.empty? ? default_title : page_title + ' | ' + default_title
+  end
 end
