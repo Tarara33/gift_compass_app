0e1d332bd1ca3329aaba974396c7cf2bf4e8938b
Sarina <sarina0618@gmail.com>
2023-12-08 23:45:51 +0900

    ✨ : ドメイン追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << 'gift-compass.com'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { protocol: 'https', host: 'gift-compass.onrender.com'}
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'gift-compass.com'}
   config.action_mailer.smtp_settings = {
     port:                 587,
     address:              'smtp.gmail.com',
