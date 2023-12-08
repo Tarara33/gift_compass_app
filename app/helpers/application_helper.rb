18d6e1eef5791d00b597844e410f5f8a01cb4392
Sarina <sarina0618@gmail.com>
2023-12-08 22:33:27 +0900

    🐛 :共有画像表示されない

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ def default_meta_tags
         card: 'summary',
         title: 'Gift Compass',
         description: 'Gift Compassはあなたのギフト選びをサポートします。',
-        image: image_url('/app/assets/images/logo.jpg'),
+        image: image_url('summary_icon.png'),
       }
     }
   end
