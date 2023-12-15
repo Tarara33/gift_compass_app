6fac5d537edfefea9702d2c7564c7af9df1290d1
Sarina <sarina0618@gmail.com>
2023-12-15 17:33:05 +0900

    🎨 : ホバー時に隠れているタグを表示する

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,3 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-
-// tooltipの有効化、初期化
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
